4bd5c5d7b2ed5f7a490a56801a1f53b1a2b085f7
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-09-01 21:06:47 +0200

    Update list_index.py

--- list_index.py
+++ list_index.py
@@ -1,6 +1,7 @@
 squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
 print(squares[::2])    # [0, 4, 16, 36, 64]
-print(squares[1::4])   # [1, 25, 81] 
+print(squares[1::4])   # [1, 25, 81]
+print(squares[::-1])   # reverse the list !!!
 print(squares[2:8:3])  # [4, 25]
 print(squares[7:5:-1]) # [49, 36]
