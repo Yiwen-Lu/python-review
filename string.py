5f7108598f123b0303e777eeccced5369ba6593a
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-09-01 20:55:51 +0200

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+print(",".join(["spam", "eggs", "ham"]))
+# spam, eggs, ham
+print("Hello ME".replace("ME", "world"))
+# Hello world
+print("This is a sentence.".startswith("This")) # NOT startwith !!!
+# True
+print("This is a sentence.".endswith("sentence.")) # NOT endwith !!!
+# True
+print("This is a sentence.".upper())
+# THIS IS A SENTENCE.
+print("AN ALL CAPS SENTENCE".lower())
+# an all caps sentence
+print("spam, eggs, ham".split(","))
+# ['spam', 'eggs', 'ham'] 
