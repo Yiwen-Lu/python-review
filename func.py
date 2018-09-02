071b052eb7413bb0c5720420ef690ee8afdc879d
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-09-02 13:35:07 +0200

    Update func.py

--- func.py
+++ func.py
@@ -7,4 +7,12 @@ def do_twice(func, x, y):
 a = 5
 b = 10
 
-print(do_twice(add, a, b))
+print(do_twice(add, a, b)) # output: 30
+
+def add_five(x):
+	return x + 5
+
+def apply_twice(func, arg):
+	return func(func(arg))
+
+print(apply_twice(add_five, b)) # output: 20
